94a71f1a1e09072eb0a82ced925442927aedc6e1
sxhyxnchxng <sxhyxnchxng@gmail.com>
2017-12-14 17:19:18 +0900

    움직임, 마디 늘어남, 먹이 랜덤 위치에 나타나게, 벽에 부딪히면 게임오버 어설프게 구현

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -3,63 +3,80 @@ import { ROWS, COLS } from './config';
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
   this.joints = [
-    { x: 1, y: 0 },
-    { x: 0, y: 0 }
+    { x: 5, y: 3 },
+    { x: 4, y: 3 },
+    { x: 3, y: 3 },
+    { x: 2, y: 3 }
   ];
 
   // 먹이의 좌표
-  this.fruit = { x: 0, y: 14 };
+  this.fruit = { x: 4, y: 4 };
 }
+
+
 
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
   console.log('up');
+  this.direction = 'up';
 }
 SnakeGameLogic.prototype.down = function() {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
   console.log('down');
+  this.direction = 'down';
 }
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
   console.log('left');
+  this.direction = 'left';
 }
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
   console.log('right');
+  this.direction = 'right';
 }
 SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
   // 게임이 아직 끝나지 않았으면 `true`를 반환
   // 게임이 끝났으면 `false`를 반환
   console.log('nextState');
-  return true;
+
+  // 이동
+  // 방향
+  let head = this.joints[0];
+  // 머리 위치
+
+  switch (this.direction) {
+    case 'up':
+      this.joints.unshift({ x: head.x, y: head.y - 1 });
+      break;
+    case 'down':
+      this.joints.unshift({ x: head.x, y: head.y + 1 });
+      break;
+    case 'left':
+      this.joints.unshift({ x: head.x - 1, y: head.y });
+      break;
+    default:
+      this.joints.unshift({ x: head.x + 1, y: head.y });
+  }
+  this.joints.pop();
+
+
+
+  // 먹이를 먹으면 늘어나는 거
+  if (head.x === this.fruit.x && head.y === this.fruit.y) {
+    this.joints.push({ x: this.joints[this.joints.length - 1].x, y: this.joints[this.joints.length - 1].y });
+    // 랜덤한 위치에 먹이 재소환
+    this.fruit = { x: Math.floor(Math.random() * COLS), y: Math.floor(Math.random() * ROWS) };
+  }
+
+
+  // 벽에 박으면 꽝
+  if (head.x > COLS - 1 || head.x < 0 || head.y > ROWS - 1 || head.y < 0) {
+    return false;
+  } else {
+    return true;
+  }
+
 }
-
-// SnakeGameLogic.prototype = {
-//   up() {
-//     // 위쪽 화살표 키를 누르면 실행되는 함수
-//     let a = 1;
-
-//   },
-//   down() {
-//     // 아래쪽 화살표 키를 누르면 실행되는 함수
-
-//   },
-//   left() {
-//     // 왼쪽 화살표 키를 누르면 실행되는 함수
-//     console.log('left');
-//   },
-//   right() {
-//     // 오른쪽 화살표 키를 누르면 실행되는 함수
-//     console.log('right');
-//   },
-//   nextState() {
-//     // 한 번 움직여야 할 타이밍마다 실행되는 함수
-//     // 게임이 아직 끝나지 않았으면 `true`를 반환
-//     // 게임이 끝났으면 `false`를 반환
-//     console.log(`nextState`);
-//     return true;
-//   },
-// }
-
 export default SnakeGameLogic;
--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const GAME_ROOT = '#game-root';
 export const AUTO_START = true;
 export const ROWS = 15;
 export const COLS = 20;
-export const INITIAL_DELAY = 2000;
-export const DELAY_EXPONENT = 1;
+export const INITIAL_DELAY = 300;
+export const DELAY_EXPONENT = 0.99;
