ca9247547e5830eba1f1d1c8cc205782800750a3
sxhyxnchxng <sxhyxnchxng@gmail.com>
2017-12-15 17:18:11 +0900

    this.를 안쓰다니..

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -11,28 +11,28 @@ function SnakeGameLogic() {
   // 먹이의 좌표
   this.fruit = { x: 3, y: 5 };
 
-  direction = 'right';
+  this.direction = 'right';
 }
 
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
   console.log('up');
-  direction = 'up';
+  this.direction = 'up';
 }
 SnakeGameLogic.prototype.down = function() {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
   console.log('down');
-  direction = 'down';
+  this.direction = 'down';
 }
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
   console.log('left');
-  direction = 'left';
+  this.direction = 'left';
 }
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
   console.log('right');
-  direction = 'right';
+  this.direction = 'right';
 }
 SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
